72a0cb5208a15b6eeb1ac1cae4aaca413acb8c88
Wachira Nduati <wachiranduati@gmail.com>
2025-02-11 20:39:23 +0300

    push to vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'Sans-serif'],
+        alata: ['Alata']
+      },
+      letterSpacing: {
+        widest: '.3em',
+      }
+    },
   },
   plugins: [],
 }
